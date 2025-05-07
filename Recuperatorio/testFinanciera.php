5638d024cb3fd58d3cc2fa0842eff7623b0cbe56
Facundo Morales <92228122+facundonmorales@users.noreply.github.com>
2025-05-07 18:28:39 -0300

    Update testFinanciera.php

--- Recuperatorio/testFinanciera.php
+++ Recuperatorio/testFinanciera.php
@@ -5,7 +5,7 @@ include_once 'Cuota.php';
 include_once 'Prestamo.php';
 
 function testSistemaFinanciera() {
-    $financiera = new Financiera("Electrodomésticos Finance", "Av. Principal 123");
+    $financiera = new Financiera("Electrodomésticos Neuquen", "Av. Argentina 123");
     
     $cliente1 = new Cliente("Facundo", "Morales", "12345678", "Calle 1", "Facundo@email.com", "111-222-333", 100000);
     $cliente2 = new Cliente("Nahuel", "Rivas", "87654321", "Calle 2", "Nahuel@email.com", "444-555-666", 80000);
